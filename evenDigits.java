1a3b88c0cb43c090f3aac119077f02844302e854
Divyansh Harne <divyanshsanjayharne23@gmail.com>
2024-07-28 20:31:28 +0530

    update

--- evenDigits.java
+++ evenDigits.java
@@ -1,6 +1,6 @@
 public class evenDigits {
     public static void main(String[] args){
-        int[] bucket = {10, 111, 10, 69, 56565, 554, 41, 29};
+        int[] bucket = {10, 111, 10, -69, 56565, -5454, 41, 29};
         int counter = 0;
         for (int i = 0; i < bucket.length; i++) {
             if(even(bucket[i])){
@@ -11,6 +11,8 @@ public class evenDigits {
     }
     static boolean even(int num){
         int count = 0;
+        if(num<0) num  = num*-1;
+        if(num == 0) return false;
         while(num>0){
             count++;
             num /=10;
