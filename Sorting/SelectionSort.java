3682972949f12d6b7077725d8e2c5d44b5743d2a
Divyansh Harne <divyanshsanjayharne23@gmail.com>
2024-10-22 16:06:17 +0530

    Selection Sort Added 22-10-2024

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,47 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {1,5,2,3,4};
+        int[] descArray = {90,75,65,43,42,39,37,33,24,12,11,10,2,-4,-15,-30};
+        System.out.println(Arrays.toString(selection(descArray)));
+    }
+    static int getMaxIndex(int[] arr, int start, int end){
+        int max = start;
+        for(int i = 0; i < end; i++){
+            if(arr[i] > arr[max]){
+                max = i;
+            }
+        }return max;
+    }
+    static int[] selection(int[] arr){
+        //here we select max/min element of the array, putting it in correct place
+        boolean swapped;
+        int l= arr.length;
+        for(int i = 0; i < l; i++){
+            int last  = l-i-1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+
+
+
+
+            // int max= 0;
+            // int maxIndex= 0;
+            // swapped = false;
+            // for(int j = 0; j < l-i; j++){
+            //     if(arr[j]> max){
+            //         max = arr[j];
+            //         maxIndex = j;
+            //     }
+            // }
+            // //SWAP
+            arr[last] = arr[last] + arr[maxIndex];
+            arr[maxIndex] = arr[last] - arr[maxIndex];
+            arr[last] = arr[last] - arr[maxIndex];
+        }
+        return arr;
+
+    }
+}
